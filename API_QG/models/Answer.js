acb522d5ccf45381d66db0d742610fb8068d6f3f
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-09-05 10:18:11 +0200

    Add getter and setter

--- API_QG/models/Answer.js
+++ API_QG/models/Answer.js
@@ -3,4 +3,12 @@ class Answer {
     constructor(text) {
         this._text = text;
     }
+
+    get text() {
+        return this._text;
+    }
+
+    set text(value) {
+        this._text = value;
+    }
 }
