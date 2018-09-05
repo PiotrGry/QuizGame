80854ba35ffb501845362f152b97b18121ab6bda
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-09-05 10:18:41 +0200

    Create Category model class

---
+++ API_QG/models/Category.js
@@ -0,0 +1,14 @@
+class Category {
+
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+}
