e1dd1da1a058287821f5e7896fe641597d91342a
PiotrGry <piotrgryzlo@gmail.com>
2018-09-05 14:31:40 +0200

    add uri to get question

--- API_QG/app.js
+++ API_QG/app.js
@@ -60,7 +60,9 @@ app.get("/categories",(req,res)=> {
 
     pool.connect((err, client, done) => {
         if (err) throw err;
-        client.query('SELECT category_name FROM categories;', (err, result) => {
+        client.query('select category_name from categories\n' +
+            'order by random()\n' +
+            'limit 1', (err, result) => {
             done();
             if (err) {
                 console.log(err.stack)
@@ -71,6 +73,32 @@ app.get("/categories",(req,res)=> {
         })
     })
 });
+app.get("/categories/:category_name",(req,res)=> {
+    pool.connect((err, client, done) => {
+        if (err) throw err;
+
+        const getQuestionQuery = `select q.question_description, a.answer_text as correct_answer from questions q
+        join categories c2 on q.category_id = c2.category_id
+        join answers a on q.correct_answer_id = a.answer_id
+        where category_name = '${req.params.category_name}'
+        order by random()
+        limit 1`;
+
+        client.query(getQuestionQuery, (err, result) => {
+            done();
+            if (err) {
+                console.log(console.error(err));
+            } else {
+                res.send(result.rows);
+
+            }
+        });
+    })
+});
+// app.get("/categories/:category_name",(req,res)=> {
+//     res.send(req.params.category_name);
+// });
+//
 
 const server = app.listen(8080, function () {
     var host = server.address().address;
