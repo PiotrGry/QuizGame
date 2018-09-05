c83d5c123f0bdee59309c521b95e2b16015a81ac
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-09-05 10:05:54 +0200

    Create User class

--- API_QG/models/User.js
+++ API_QG/models/User.js
@@ -1,5 +1,23 @@
 class User{
-    constructor(nick){
-        this.nick = nick;
+
+    constructor(nick, score){
+        this._nick = nick;
+        this._score = score;
+    }
+
+    get nick() {
+        return this._nick;
+    }
+
+    set nick(value) {
+        this._nick = value;
+    }
+
+    get score() {
+        return this._score;
+    }
+
+    set score(value) {
+        this._score = value;
     }
 }
