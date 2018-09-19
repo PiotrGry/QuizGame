0dde963d383abc37d63be4e7ecdbc663828d8451
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-09-19 11:15:32 +0200

    Change import

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
 const should = chai.should();
-const  pool = require("../db/dbConfigNoPoolConnection");
+const  pool = require("../db/db");
 
 
 
