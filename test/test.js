24c8fbcb290aa7a8dfb6f219e1227f3c2814c9e0
PiotrGry <piotrgryzlo@gmail.com>
2018-09-19 09:57:49 +0200

    refactor in test.js

--- test/test.js
+++ test/test.js
@@ -4,38 +4,45 @@ const server = require('../app');
 const should = chai.should();
 const  pool = require("../db/dbConfigNoPoolConnection");
 
-
-
-pool.query('SELECT 1+1 as solution', (error, results) => {
-    if (error) throw error;
-    console.log(results.rows[0].solution);
-});
-
+const address = 'http://localhost:8080';
 chai.use(chaiHttp);
 
-describe('app', ()=> {
+
+describe('dbConnection', ()=> {
+    it('Database connection');
+
+
+    it('Database connection', (done) => {
+        pool.query('SELECT 1 + 1 AS solution', (error, results) => {
+            if (error) throw error;
+            chai.assert.equal(results.rows[0].solution, 2);
+            done();
+        });
+    });
+});
+
+
+describe('root rout', ()=> {
     it('should get status 200');
 
     it('should get status 200', (done) => {
-        chai.request('http://localhost:8080')
+        chai.request(address)
             .get('/')
             .end((err, res) => {
-                res.should.have.status(200);
                 done();
+                res.should.be.status(200);
             })
     });
 });
 
-describe('dbConnection', ()=> {
-    it('should receive one row with record: 2 ');
-    it('should get not 2');
-
-    it('should receive one row with record: 2', () => {
-
-        pool.query('SELECT 1 + 1 AS solution', (error, results) => {
-            if (error) throw error;
-            chai.assert.equal(results.rows[0].solution, "1");
-        });
+describe("categories/:category_name/question",()=> {
+    it("should get question, correct answer and all answers", (done) => {
+        chai.request(address)
+            .get("categories/zwierzeta/question")
+            .end((err, res) => {
+                done();
+                chai.assert.equal(res, " ")
+            });
     });
 
     it("should get not 2 ",()=>{
@@ -43,3 +50,4 @@ describe('dbConnection', ()=> {
         chai.assert.notEqual(result, 2, "result is 4");
     })
 });
+
