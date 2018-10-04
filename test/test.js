dca8ecbf85311138fb9230f566f8f900a5190752
PiotrGry <piotrgryzlo@gmail.com>
2018-10-04 16:55:10 +0200

    refactor in tests and add test to highscores

--- test/test.js
+++ test/test.js
@@ -11,7 +11,6 @@ chai.use(chaiHttp);
 describe('dbConnection', ()=> {
     it('Database connection');
 
-
     it('Database connection', (done) => {
         pool.query('SELECT 1 + 1 AS solution', (error, results) => {
             if (error) throw error;
@@ -35,12 +34,11 @@ describe('root rout', ()=> {
 });
 
 
-describe("Unexisting paths",()=> {
-
+describe("categories",()=> {
     it("should get status 404 when requested category is missing");
+    it("should get all 8 questions from category");
 
     it("should get status 404 when requested category is missing", (done) => {
-
         chai.request(address)
             .get("/categories/someStrangeSignsWhichRepresentUnexistingNameOfCategory/question")
             .end((err, res) =>{
@@ -49,14 +47,35 @@ describe("Unexisting paths",()=> {
             });
     });
 
-});
-describe("test json content",()=>{
-    it("should get all 8 questions from category");
     it("should get all 8 questions from category",(done) =>{
+        const expectedLengh = 8;
         chai.request(address)
             .get("/categories/zwierzeta/question")
             .end((err, res) =>{
-            chai.assert.strictEqual(res.body.length,8);
+                chai.assert.strictEqual(res.body.length, expectedLengh);
+                done();
+            });
+    });
+
+
+});
+describe("high scores",()=>{
+    it("should get 10 users in descending order");
+    it("should get \"Kamil\"as best player" );
+
+    it("should get 10 users in descending order",(done)=>{
+        chai.request(address)
+            .get("/highscores")
+            .end((err, res) =>{
+                chai.assert.strictEqual(res.body.length, 10);
+                done();
+            });
+    });
+    it("should get \"Kamil\" as best player",(done)=>{
+        chai.request(address)
+            .get("/highscores")
+            .end((err, res) =>{
+                chai.assert.strictEqual(res.body[0]._nick, "Kamil");
                 done();
             });
     });
