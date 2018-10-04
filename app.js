553ee9b3fbed74d5bd7e771137d55ba6a547c072
PiotrGry <piotrgryzlo@gmail.com>
2018-10-04 16:04:28 +0200

    add rout to show high score

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.get("/users",(req, res)=> {
     playerCon.findUsers(req, res);
 });
 
+app.get("/users/highscores",(req, res)=> {
+    playerCon.findHighscores(req, res);
+});
+
 app.get("/categories",(req,res)=> {
    categoryCon.getAllCategories(req, res);
 });
