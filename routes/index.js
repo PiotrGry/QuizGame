54c09d5c80b8c7eb0df1d53e3c5012f1ab031814
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-05 11:22:29 +0200

    Add /categories route to router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const playerController = require('../controllers').player;
+const playerController = require('../controllers/player');
+const categoryController = require('../controllers/category');
 const router = express.Router();
 
 /* GET home page. */
@@ -7,6 +8,10 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+//PLAYERS ROUTES
 router.get('/players', playerController.list);
+
+//CATEGORIES ROUTES
+router.get('/categories', categoryController.list);
 
 module.exports = router;
