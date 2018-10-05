8f359a927f9b62f1a80d94b9f4d7259b603262e9
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-05 12:16:36 +0200

    Add seeds

--- seeders/20181005094230-demo-question.js
+++ seeders/20181005094230-demo-question.js
@@ -1,18 +1,17 @@
 'use strict';
-
 const uuidv4 = require('uuid/v4');
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Players', [{
+        return queryInterface.bulkInsert('Questions', [{
             id: uuidv4(),
-            player_nick: 'Salemcio',
+            question_description: 'How many legs does a kitty have?',
             created_at: Sequelize.fn('NOW'),
             updated_at: Sequelize.fn('NOW'),
         }], {});
     },
 
     down: (queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Players', null, {});
+        return queryInterface.bulkDelete('Questions', null, {});
     }
 };
--- seeders/20181005101022-demo-answer.js
+++ seeders/20181005101022-demo-answer.js
@@ -1,18 +1,17 @@
 'use strict';
-
 const uuidv4 = require('uuid/v4');
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Players', [{
+        return queryInterface.bulkInsert('Answers', [{
             id: uuidv4(),
-            player_nick: 'Salemcio',
+            answer_text: 'Five',
             created_at: Sequelize.fn('NOW'),
             updated_at: Sequelize.fn('NOW'),
         }], {});
     },
 
     down: (queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Players', null, {});
+        return queryInterface.bulkDelete('Answers', null, {});
     }
 };
