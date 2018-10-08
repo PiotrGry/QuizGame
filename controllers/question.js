cb8a2910e8af5da6352d860b93c1730fa2a52f59
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-08 13:33:57 +0200

    Add findOne method

--- controllers/question.js
+++ controllers/question.js
@@ -7,5 +7,20 @@ module.exports = {
             .catch((error) => {
                 res.status(400).send(error);
             });
+    },
+
+    findOne(req, res) {
+        return Question.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then((question) => res.status(200).send(question))
+            .catch((error) => {
+                res.status(400).send(error);
+            });
     }
 };
+
+
+//findOne
+//pod dodawaniu answer trzeba wyciągnąć question i dodać correct_answer_id do niego ale także do answer trzeba dodać question_id więc findOne się motzno przyda
