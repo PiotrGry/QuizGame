d32fa55ee7678bc7bc8e9aad339e626955f3d6c9
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-08 15:37:22 +0200

    Add question association in category seeder

--- seeders/20181005090133-demo-category.js
+++ seeders/20181005090133-demo-category.js
@@ -2,16 +2,29 @@
 const uuidv4 = require('uuid/v4');
 
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Categories', [{
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert('categories', [{
             id: uuidv4(),
             category_name: 'Kittieeees',
             created_at: Sequelize.fn('NOW'),
             updated_at: Sequelize.fn('NOW'),
         }], {});
+
+        const categories = await queryInterface.sequelize.query(`SELECT id FROM categories;`);
+
+        const id = categories[0][0].id;
+
+        return await queryInterface.bulkInsert('questions', [{
+            id: uuidv4(),
+            question_description: 'How many legs does cats have?',
+            category_id: id,
+            created_at: Sequelize.fn('NOW'),
+            updated_at: Sequelize.fn('NOW'),
+        }], {});
     },
 
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Categories', null, {});
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkDelete('questions', null, {});
+        await queryInterface.bulkDelete('categories', null, {});
     }
 };
