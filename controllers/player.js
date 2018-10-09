55eed3db545daaa13ba530bc4758f54e58890fe0
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-09 13:03:06 +0200

    Add listHighscores method

--- controllers/player.js
+++ controllers/player.js
@@ -7,5 +7,18 @@ module.exports = {
             .catch((error) => {
                 res.status(400).send(error);
             });
+    },
+
+    listHighscores(req, res) {
+        return Player.findAll({
+            order: [
+                ['player_score', 'DESC']
+            ],
+            limit: 5
+            }
+        ).then((players) => res.status(200).send(players))
+            .catch((error) => {
+                res.status(400).send(error);
+            });
     }
 };
