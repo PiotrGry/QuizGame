5743a24db4169e644822513f2b9992cdd5a1c84d
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-17 11:13:45 +0200

    Change tables names in migrations

--- migrations/02-create-category.js
+++ migrations/02-create-category.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('categories', {
+    return queryInterface.createTable('Categories', {
       id: {
         primaryKey: true,
         type: Sequelize.UUID,
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('categories');
+    return queryInterface.dropTable('Categories');
   }
 };
