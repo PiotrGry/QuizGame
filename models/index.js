dc4b900494857f9d2228ad0116c1b0b4b3146cf6
PiotrGry <piotrgryzlo@gmail.com>
2018-10-17 15:16:47 +0200

    refactor in index.js

--- models/index.js
+++ models/index.js
@@ -4,14 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
 let sequelize;
 
-// if(config.use_env_variable){
-
+if(process.env.DATABASE_URL){
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
         dialectOptions: {
@@ -19,9 +15,11 @@ let sequelize;
         },
         quoteIdentifiers: false
     });
-// } else {
-//     sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+} else {
+    const env = process.env.NODE_ENV || 'development';
+    const config = require(__dirname + '/../config/config.json')[env];
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
