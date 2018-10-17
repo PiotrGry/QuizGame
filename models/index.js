0faf7518f82fedb5f43e23509db88c360cf71f80
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-17 13:01:15 +0200

    Add dialectOptions to sequelize

--- models/index.js
+++ models/index.js
@@ -8,24 +8,21 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-// console.log(config.use_env_variable);
-// console.log(env)
-console.log(process.env);
 let sequelize;
 if(config.use_env_variable){
-  console.log("wchodzi")
-    var db_info = process.env["DATABASE_URL"].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-    config.dialect=db_info[1];
-    config.username=db_info[2];
-    config.password=db_info[3];
-    config.host=db_info[4];
-    config.port=db_info[5];
-    config.database=db_info[6];
+
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: true
+        }
+    });
 } else {
-  console.log("niee")
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
 
 fs
   .readdirSync(__dirname)
