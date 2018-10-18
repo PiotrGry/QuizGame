dfb8db26708e8b16922998c0cc618c7dec8d39cd
PiotrGry <piotrgryzlo@gmail.com>
2018-10-18 16:19:40 +0200

    only 404 test failure

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../app');
 const db = require("../models/index.js");
+const server = require('../app');
 const should = chai.should();
-const address = 'http://localhost:8080';
 chai.use(chaiHttp);
 
 
@@ -25,61 +24,61 @@ describe('root rout', ()=> {
     it('should get status 200');
 
     it('should get status 200', (done) => {
-        chai.request(address)
+        chai.request(server)
             .get('/')
             .end((err, res) => {
                 done();
                 res.should.be.status(200);
+        });
+    });
+});
+
+
+    describe("categories",()=> {
+        it("should get status 404 when requested category is missing");
+        it("should get all 8 questions from category");
+
+        it("should get status 404 when requested category is missing", (done) => {
+            chai.request(server)
+                .get("/categories/someStrangeSignsWhichRepresentUnexistingNameOfCategory/question")
+                .end((err, res) =>{
+                    res.should.be.status(404);
+                    done()
+                });
+        });
+
+    it("should get all 8 questions from category",(done) =>{
+        const expectedLength = 8;
+        chai.request(server)
+            .get("/categories/485af8be-c38c-4e80-a135-d75223a81817/questions")
+            .end((err, res) =>{
+                chai.assert.strictEqual(res.body.questions.length, expectedLength);
+                done();
+            });
+    });
+
+
+});
+describe("high scores",()=>{
+    it("should get 10 users in descending order");
+    it("should get \"Kamil\"as best player" );
+
+    it("should get 5 users in descending order",(done)=>{
+        chai.request(server)
+            .get("/highscores")
+            .end((err, res) =>{
+                chai.assert.strictEqual(res.body.length, 5);
+                done();
+            });
+    });
+    it("should get \"Kamil\" as best player",(done)=>{
+        chai.request(server)
+            .get("/highscores")
+            .end((err, res) =>{
+                chai.assert.strictEqual(res.body[0].player_nick, "Kamil");
+                done();
             });
     });
 });
-//
-//
-// describe("categories",()=> {
-//     it("should get status 404 when requested category is missing");
-//     it("should get all 8 questions from category");
-//
-//     it("should get status 404 when requested category is missing", (done) => {
-//         chai.request(address)
-//             .get("/categories/someStrangeSignsWhichRepresentUnexistingNameOfCategory/question")
-//             .end((err, res) =>{
-//                 res.should.be.status(404);
-//                 done()
-//             });
-//     });
-//
-//     it("should get all 8 questions from category",(done) =>{
-//         const expectedLengh = 8;
-//         chai.request(address)
-//             .get("/categories/zwierzeta/question")
-//             .end((err, res) =>{
-//                 chai.assert.strictEqual(res.body.length, expectedLengh);
-//                 done();
-//             });
-//     });
-//
-//
-// });
-// describe("high scores",()=>{
-//     it("should get 10 users in descending order");
-//     it("should get \"Kamil\"as best player" );
-//
-//     it("should get 10 users in descending order",(done)=>{
-//         chai.request(address)
-//             .get("/highscores")
-//             .end((err, res) =>{
-//                 chai.assert.strictEqual(res.body.length, 10);
-//                 done();
-//             });
-//     });
-//     it("should get \"Kamil\" as best player",(done)=>{
-//         chai.request(address)
-//             .get("/highscores")
-//             .end((err, res) =>{
-//                 chai.assert.strictEqual(res.body[0]._nick, "Kamil");
-//                 done();
-//             });
-//     });
-// });
 
 
