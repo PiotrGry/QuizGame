e5f1bcf24538f2bd2b7e391714f78dae32dbdf3f
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-10-19 14:39:44 +0200

    Add adding answers to question in add method (before refactoring)

--- controllers/question.js
+++ controllers/question.js
@@ -1,4 +1,6 @@
 const Question = require("../models").Question;
+const Answer = require("../models").Answer;
+const uuid = require('uuid/v4');
 
 module.exports = {
     list(req, res) {
@@ -20,6 +22,15 @@ module.exports = {
             });
     },
 
+//Method add needs JSON like this:
+// {
+//     "question_description": "Ile kot ma uszu",
+//     "correct_answer": "2",
+//     "wrong_answer_1": "3",
+//     "wrong_answer_2": "4",
+//     "wrong_answer_3": "5"
+// }
+
     add(req, res) {
         const categoryId = req.params.id;
         const questionDescription = req.body.question_description;
@@ -28,10 +39,52 @@ module.exports = {
                 question_description: questionDescription,
                 category_id: categoryId,
             }
-        ).then((category) => {
-            res.status(201).send(category)
+        ).then((question) => {
+            const questionId = question.dataValues.id;
+            const correctAnswerText = req.body.correct_answer;
+
+            return Answer.create({
+                answer_text: correctAnswerText,
+                question_id: questionId,
+            })
+        }).then(answer => {
+            const answerId = answer.dataValues.id;
+            const questionId = answer.dataValues.question_id;
+
+            return Question.update(
+                {correct_answer_id: answerId},
+                {returning: true,
+                    where:
+                        {id: questionId}
+                },
+            )
         })
-            .catch(error => {
+            .then(question => {
+            const questionId = question[1][0].dataValues.id;
+            const wrongAnswer1 = req.body.wrong_answer_1;
+            const wrongAnswer2 = req.body.wrong_answer_2;
+            const wrongAnswer3 = req.body.wrong_answer_3;
+
+            return Answer.bulkCreate([{
+                id: uuid(),
+                answer_text: wrongAnswer1,
+                question_id: questionId
+            },
+                {
+                    id: uuid(),
+                    answer_text: wrongAnswer2,
+                    question_id: questionId
+                },{
+                    id: uuid(),
+                    answer_text: wrongAnswer3,
+                    question_id: questionId
+                }
+            ])
+        })
+            .then(answers => {
+            res.status(201).send(answers);
+        }
+        ).catch(error => {
                 res.status(400).send(error.message);
             })
     }
